040a5808ef4b1f11d2dc046835fc2c2203f9880e
Joshua Litam <joshua_litam@yahoo.com>
2023-10-20 12:30:39 +0800

    misc commit

--- app/components/AboutUs.jsx
+++ app/components/AboutUs.jsx
@@ -13,11 +13,11 @@ const brands = [
 
 const AboutUs = () => {
   return (
-    <div className="about-us min-h-3/4 min-w-full h-full flex mt-32 about-us-wrapper justify-center px-16">
+    <div className="about-us min-h-3/4 min-w-full h-full flex mt-48 about-us-wrapper justify-center px-16">
       <div className="about-us-container max-w-7xl flex flex-col w-full">
-        <h2 className='text-3xl font-bold about-us-title text-red-500'>About Us</h2>
+        <h2 className='text-2xl font-bold about-us-title text-red-500'>About Us</h2>
         <div className="mt-4 about-us-text flex">
-          <div className="about-us-left text-4xl font-black  flex-1">Big Joe Inc offers end-to-end services for all your event needs</div>
+          <div className="about-us-left text-5xl font-black  flex-1">Big Joe Inc offers end-to-end services for all your event needs</div>
           <div className="about-us-right text-xl font-bold text-right text-gray-500 flex-1">
             <p className='about-us-p pl-36'> With almost 10 years of event experience, Big Joe Inc serves as the go-to agency for
               accomplishing all event requirements.
@@ -33,7 +33,6 @@ const AboutUs = () => {
             </div>
           </div>
       </div>
-
     </div>
   )
 }
