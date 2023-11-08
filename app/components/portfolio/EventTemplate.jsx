b31b7853af233f6705b490a84f557ef1b21ae519
Joshua Litam <joshua_litam@yahoo.com>
2023-11-08 10:17:54 +0800

    added slider to portfolio carousel

--- app/components/portfolio/EventTemplate.jsx
+++ app/components/portfolio/EventTemplate.jsx
@@ -12,29 +12,32 @@ const EventTemplate = () => {
   const event = items.filter((item) => (
     item.id == search
   ))
+  const imageSet = event[0].supportingImages
 
   const [page, setPage] = useState(1)
 
   const setPrev = () => {
-    setPage(prev => ((prev - 1) > 0 ? prev - 1 : 3))
+    setPage(prev => ((prev - 1) > 0 ? prev - 1 : imageSet.length))
   }
 
   const setNext = () => {
-    setPage(prev => ((prev + 1) >= 4 ? 1 : prev + 1))
+    setPage(prev => ((prev + 1) >= imageSet.length + 1 ? 1 : prev + 1))
   }
 
   return (
     <div className='w-screen h-screen flex justify-center items-center'>
       <div className="flex justify-center item-center max-w-7xl">
         <div className="w-full event-container flex flex-col gap-4">
-          <Image src={event[0].supportingImages[page - 1]} width={800} height={700} className='border-4 border-grey rounded-lg duration-700 ease-in-out' />
-          <div className="bg-gray-200 h-32 w-full rounded-lg flex justify-around items-center">
-            <button onClick={setPrev}>left</button>
-            {event[0].supportingImages.map((image) => (
-              <Image src={image} width={150} height={175} />
+
+          <Image src={imageSet[page - 1]} width={800} height={700} alt={imageSet[page - 1].eventName} className='border-4 border-grey rounded-lg duration-700 ease-in-out' />
+          <div className="flex justify-center items-center gap-8">
+            <button onClick={setPrev}><Image src={"/img/left.png"} width={40} height={40}/></button>
+            {imageSet.map((image, i) => (
+              <Image src={(i == page - 1) ? "/img/circle.png" : "/img/oval.png"} width={20} height={20} className={i}/>
             ))}
-            <button onClick={setNext}>right</button>
+            <button onClick={setNext}><Image src={"/img/right.png"} width={40} height={40}/></button>
           </div>
+
         </div>
         <div className='flex flex-col justify-center items-start gap-8 ml-16'>
           <h1 className='text-5xl text-black font-bold'><span className='text-red-500'>{event[0].eventName}</span></h1>
