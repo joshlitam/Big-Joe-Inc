aecb6069f67897aab9254414cb9c32ea68bc1d28
Joshua Litam <joshua_litam@yahoo.com>
2023-11-08 18:12:01 +0800

    converted jpg to webp

--- app/data.js
+++ app/data.js
@@ -1,12 +1,12 @@
 const items = [
-    { "id": "gatoraderun", "eventName": "Gatorade Run", "eventDescription": "Installation of a 10' high by 20' long science booth accompanied by a team of event staff and hydration coaches.", "services": ["event activation", "installation", "fabrication"], "imageURL": "/img/hero-pattern-1.jpg", "supportingImages": [
-        "/img/hero-pattern-1.jpg", "/img/gatorade-run/1.jpg", "/img/gatorade-run/2.jpg", "/img/gatorade-run/3.jpg"
+    { "id": "gatoraderun", "eventName": "Gatorade Run", "eventDescription": "Installation of a 10' high by 20' long science booth accompanied by a team of event staff and hydration coaches.", "services": ["event activation", "installation", "fabrication"], "imageURL": "/img/hero-pattern-1.webp", "supportingImages": [
+        "/img/hero-pattern-1.webp", "/img/gatorade-run/1.webp", "/img/gatorade-run/2.webp", "/img/gatorade-run/3.webp"
     ] },
-    { "id": "pbadraft", "eventName": "PBA Draft", "eventDescription": "Fabrication of all marketing collaterals(a-boards, tarps, backdrops, etc.) as well as faciltation of the coaches drills.", "services": ["event activation", "installation", "fabrication", "sports"], "imageURL": "/img/hero-pattern-2.jpg", "supportingImages": [
-        "/img/hero-pattern-2.jpg", "/img/pba-draft/1.jpg", "/img/pba-draft/2.jpg", "/img/pba-draft/3.jpg"
+    { "id": "pbadraft", "eventName": "PBA Draft", "eventDescription": "Fabrication of all marketing collaterals(a-boards, tarps, backdrops, etc.) as well as faciltation of the coaches drills.", "services": ["event activation", "installation", "fabrication", "sports"], "imageURL": "/img/hero-pattern-2.webp", "supportingImages": [
+        "/img/hero-pattern-2.webp", "/img/pba-draft/1.webp", "/img/pba-draft/2.webp", "/img/pba-draft/3.webp"
     ]},
-    { "id": "literoflight", "eventName": "Liter of Light", "eventDescription": "Production of all marketing collaterals and facilitation of event.", "services": ["event activation", "installation", "fabrication"], "imageURL": "/img/hero-pattern-3.jpg", "supportingImages": [
-        "/img/hero-pattern-3.jpg", "/img/liter-of-light/1.jpg", "/img/liter-of-light/2.jpg", "/img/liter-of-light/3.jpg"
+    { "id": "literoflight", "eventName": "Liter of Light", "eventDescription": "Production of all marketing collaterals and facilitation of event.", "services": ["event activation", "installation", "fabrication"], "imageURL": "/img/hero-pattern-3.webp", "supportingImages": [
+        "/img/hero-pattern-3.webp", "/img/liter-of-light/1.webp", "/img/liter-of-light/2.webp", "/img/liter-of-light/3.webp"
     ] },
     
 ]
