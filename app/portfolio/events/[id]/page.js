3c4d8ac39caf04fdf647b2bee038be5ccb9b1965
Joshua Litam <joshua_litam@yahoo.com>
2023-11-14 19:39:26 +0800

    fixed description on event page

--- app/portfolio/events/[id]/page.js
+++ app/portfolio/events/[id]/page.js
@@ -10,14 +10,14 @@ const fetcher = (...args) => fetch(...args).then((res) => res.json())
 const page = () => {
     const params = useParams()
     const id = params.id
-    const { data, error } = useSWR(`${process.env.NEXT_PUBLIC_API_URL}/api/events/${id}`, fetcher)
+    const { data, error } = useSWR(`/api/events/${id}`, fetcher)
 
     if (error) return <div className='h-screen w-screen'>failed to load..</div>
     if (!data) return <div className='h-screen w-screen flex justify-center items-center'><Image src='/img/loading.png' width={200} height={200} /></div>
     const event = data.event
 
     return (
-        <Event image1={event.primaryImage} image2={event.supportingImage[0]} image3={event.supportingImage[1]} image4={event.supportingImage[2]} eventName={event.eventName} description={event.EventDescription} tags={event.tags} />
+        <Event image1={event.primaryImage} image2={event.supportingImage[0]} image3={event.supportingImage[1]} image4={event.supportingImage[2]} eventName={event.eventName} description={event.description} tags={event.tags} />
 
     )
 }
