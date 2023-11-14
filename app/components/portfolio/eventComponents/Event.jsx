45ab40fd879f9c9e460b30fc4149562d8b7a4007
Joshua Litam <joshua_litam@yahoo.com>
2023-11-14 20:28:15 +0800

    fixed admin page

--- app/components/portfolio/eventComponents/Event.jsx
+++ app/components/portfolio/eventComponents/Event.jsx
@@ -27,10 +27,10 @@ const Event = ({ image1, image2, image3, image4, eventName, description, tags })
                     <div className='et flex flex-col gap-4 w-2/3'>
                         <CldImage src={images[page - 1]} width={900} height={700} className='border-2 border-black'/>
                         <div className='flex justify-center items-center bg-gray-200 py-2 overflow-hidden'>
-                            <CldImage src={image1} width={100} height={100} className={page == 1 ? "border-2 border-green-400 et-image" : "et-image"} onClick={() => handleClick(1)}/>
-                            <CldImage src={image2} width={100} height={100} className={page == 2 ? "border-2 border-green-400 et-image" : "et-image"} onClick={() => handleClick(2)}/>
-                            <CldImage src={image3} width={100} height={100} className={page == 3 ? "border-2 border-green-400 et-image" : "et-image"} onClick={() => handleClick(3)}/>
-                            <CldImage src={image4} width={100} height={100} className={page == 4 ? "border-2 border-green-400 et-image" : "et-image"} onClick={() => handleClick(4)}/>
+                            <CldImage src={image1} width={100} height={100} className={page == 1 ? "border-2 border-green-400 et-image" : ""} onClick={() => handleClick(1)}/>
+                            <CldImage src={image2} width={100} height={100} className={page == 2 ? "border-2 border-green-400 et-image" : ""} onClick={() => handleClick(2)}/>
+                            <CldImage src={image3} width={100} height={100} className={page == 3 ? "border-2 border-green-400 et-image" : ""} onClick={() => handleClick(3)}/>
+                            <CldImage src={image4} width={100} height={100} className={page == 4 ? "border-2 border-green-400 et-image" : ""} onClick={() => handleClick(4)}/>
                         </div>
                     </div>
                     <EventDescription eventName={eventName} eventDescription={description} tags={tags} />
