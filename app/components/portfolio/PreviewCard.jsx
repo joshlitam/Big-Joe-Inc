6b24ab7259d1a76643eab777f1b59152139f7572
Joshua Litam <joshua_litam@yahoo.com>
2023-11-16 18:04:14 +0800

    fixed object fit

--- app/components/portfolio/PreviewCard.jsx
+++ app/components/portfolio/PreviewCard.jsx
@@ -5,8 +5,8 @@ import { CldImage } from 'next-cloudinary'
 
 const PreviewCard = ({imageURL, eventName }) => {
   return (
-    <div className='pc h-auto w-auto'>
-      <CldImage src={imageURL} width={500} height={400} className='border-4 border-gray-400 mb-2' alt={eventName}/>
+    <div className='pc' style={{height:"400px", width:"500px"}}>
+      <CldImage src={imageURL} width={500} height={400} className='border-4 border-gray-400 mb-2 pc-image' alt={eventName}/>
       <h2 className='text-xl font-bold text-center'>{eventName}</h2>
     </div>
   )
